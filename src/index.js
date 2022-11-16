12366730e85582ac20e3e61ae332f9872b2e4028
Gabriel Alves <gabriel.s.t.a.r.k@hotmail.com>
2022-11-16 15:46:25 -0300

    :sparkles: Criação de usuário

--- src/index.js
+++ src/index.js
@@ -1,21 +1,45 @@
 const express = require('express');
 const cors = require('cors');
-
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const check = users.some(user => user.username === username );
+
+  if(!check){
+    return response.status(400).send({error: "Usuário não encontrado."})
+  }
+  
+  return next()
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username} = request.body;
+
+  const verifyIfUsernameExist = users.some(user => user.username === username)
+
+
+  if(verifyIfUsernameExist){
+    return response.status(400).send({error: "Usuário já existe"});
+  }
+  const newUser = {
+    id: uuidv4(), 
+    name,
+    username,
+    todos: []
+  }
+
+  users.push(newUser);
+
+  return response.send();
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
